49e60ed0354729cecd005ad93dcf80f2291016e6
MSambu <sambucetti.mateo@gmail.com>
2023-09-19 02:05:05 -0300

    diseño Header

--- MystiX/src/components/Header.jsx
+++ MystiX/src/components/Header.jsx
@@ -1,5 +1,22 @@
-const Header = () => {
-  return <h1>Este es el header</h1>;
-};
+
+import search from '../assets/search.svg'
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul><li><span className='LogoM'>Mysti</span><span className='LogoX'>X</span></li></ul>
+        </nav>
+
+        <h2>
+          <div className='Hcontainer'>
+            <input type="text" className='Hsearch' placeholder="Buscar..." />
+          </div>
+        </h2>
+      </header>
+    </>
+  );
+}
 
 export default Header;
